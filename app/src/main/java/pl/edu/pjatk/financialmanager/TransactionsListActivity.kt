eac87a8a09b81abd3535278d550a0003a778d806
emstryszawski <stryszawskiemil@gmail.com>
2022-11-11 19:23:35 +0100

    RecyclerView works

---
+++ app/src/main/java/pl/edu/pjatk/financialmanager/TransactionsListActivity.kt
@@ -0,0 +1,35 @@
+package pl.edu.pjatk.financialmanager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import pl.edu.pjatk.financialmanager.databinding.ActivityTransactionsListBinding
+
+class TransactionsListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTransactionsListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTransactionsListBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.transactionsRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        val transactionsAdapter = TransactionsAdapter(view.context) { transactionOnClick() }
+
+        binding.transactionsRecyclerView.adapter = transactionsAdapter
+
+//        binding.newTransactionFab.setOnClickListener {
+//            addNewTransaction()
+//        }
+    }
+
+    private fun transactionOnClick() {
+        print("click")
+    }
+
+    private fun addNewTransaction() {
+        TODO("Not yet implemented")
+    }
+}
